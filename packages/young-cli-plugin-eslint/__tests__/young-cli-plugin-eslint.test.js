976e666626e970132efdf219658d82efa84cd420
yongqing_luo <yongqing_luo@kingdee.com>
2021-11-04 19:08:44 +0800

    feat: young create

---
+++ packages/young-cli-plugin-eslint/__tests__/young-cli-plugin-eslint.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const youngCliPluginEslint = require('..');
+
+describe('young-cli-plugin-eslint', () => {
+    it('needs tests');
+});
---
+++ packages/young-cli-plugin-vuex/__tests__/young-cli-plugin-vuex.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const youngCliPluginVuex = require('..');
+
+describe('young-cli-plugin-vuex', () => {
+    it('needs tests');
+});
